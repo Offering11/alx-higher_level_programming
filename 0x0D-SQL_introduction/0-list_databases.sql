0475c6080aeac9d80eb8ef3add3f1dac976e0603
Offering11 <lawaloffering@gmail.com>
2023-03-14 14:32:17 -0700

    working on database

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,2 @@
--- This script lists all databases in MySQL server. The SHOW DATABASES can also to thesame as my code under.
-SELECT schema_name
-FROM information_schema.schemata;
+-- This script lists all databases in MySQL server
+SHOW DATABASES;
